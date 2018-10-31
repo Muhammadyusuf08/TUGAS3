5bd5ce92294a0bcdcf98f019096a2da3d4449e22
Muhammadyusuf08 <43636443+Muhammadyusuf08@users.noreply.github.com>
2018-10-31 22:18:26 +0700

    Create MainStackString.java

---
+++ MainStackString.java
@@ -0,0 +1,14 @@
+public class MainStackString {
+public static void main(String[] args) {
+		classstackstring st= new classstackstring();
+		
+		st.push("alief");
+    st.push("taufik");
+		st.push("abdi");
+		st.push("qadri");
+		
+		String d0=st.pop();
+		System.out.println(d0);
+		st.cetak();
+	}
+}
