5c8629024cc6ef2ffd49e542f7bc176864a02c5f
Muhammadyusuf08 <43636443+Muhammadyusuf08@users.noreply.github.com>
2018-10-31 22:19:40 +0700

    Create MainQueueString.java

---
+++ MainQueueString.java
@@ -0,0 +1,14 @@
+import sun.misc.Queue;
+public class MainQueueString {
+
+	public static void main(String[] args) {
+		QueueString q = new QueueString();
+		q.cetak();
+		
+		q.insert("MUHAMMAD");q.cetak();
+		q.insert("YUSUF");q.cetak();
+		q.insert("NAS");q.cetak();
+		q.insert("AMIN");q.cetak();
+
+	}
+}
